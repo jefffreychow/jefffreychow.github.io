0b0456f282b687d1d65b6edf90c7e0d348674b04
jefffreychow <jchow@albany.edu>
2020-07-08 04:45:35 -0400

    Added styling to tabs
    Last git had only the home page showing and the other tabs not showing up at all. Added active to script so it should work

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ tabs.forEach(tab => {
     tab.addEventListener('click', () => {
         const target = document.querySelector(tab.dataset.tabTarget)
         tabContents.forEach(tabContent => {
-            tabContent.classList.remove
+            tabContent.classList.remove('active')
         })
+        
+        tabs.forEach(tab => {
+            tab.classList.remove('active')
+        })
+        tab.classList.add('active')
         target.classList.add('active')
     })
     
